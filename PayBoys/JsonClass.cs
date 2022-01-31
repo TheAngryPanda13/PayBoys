ea66f6d457c6572b2c39fecd4ee561ab6e55b91e
TheAngryPanda13 <theangrypanda001@gmail.com>
2022-01-31 13:07:30 +0300

    Работа с SQLite

---
+++ PayBoys/JsonClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayBoys
+{
+    class JsonClass
+    {
+        public string user { get; set; }
+        public int totalSum { get; set; }
+        public DateTime localDateTime { get; set; }
+        public JsonItems[] items { get; set; }
+
+        public string nameTable;
+        public string NameTable()
+        {
+            if (this.user.Contains("Агроторг") || this.user.Contains("АГРОТОРГ"))
+            {
+                this.nameTable = "Пятёрочка " + this.localDateTime.ToString("dd.MM-hh:mm");
+                return this.nameTable;
+            }
+            if (this.user.Contains("Прайс") || this.user.Contains("ПРАЙС"))
+            {
+                this.nameTable = "ФиксПрайс " + this.localDateTime.ToString("dd.MM-hh:mm");
+                return this.nameTable;
+            }
+            if (this.user.Contains("Лента") || this.user.Contains("ЛЕНТА"))
+            {
+                this.nameTable = "Лента " + this.localDateTime.ToString("dd.MM-hh:mm");
+                return this.nameTable;
+            }
+            if (this.user.Contains("Перекресток") || this.user.Contains("ПЕРЕКРЕСТОК"))
+            {
+                this.nameTable = "Перекресток " + this.localDateTime.ToString("dd.MM-hh:mm");
+                return this.nameTable;
+            }
+            if (this.user.Contains("Магнит") || this.user.Contains("МАГНИТ"))
+            {
+                this.nameTable = "Магнит " + this.localDateTime.ToString("dd.MM-hh:mm");
+                return this.nameTable;
+            }
+            if (this.user.Contains("Дикси") || this.user.Contains("ДИКСИ"))
+            {
+                this.nameTable = "Дикси " + this.localDateTime.ToString("dd.MM-hh:mm");
+                return this.nameTable;
+            }
+            else
+                return "Error";
+        }
+    }
+}
--- PayBoys/MainWindow.xaml.cs
+++ PayBoys/MainWindow.xaml.cs
@@ -22,62 +22,18 @@ namespace PayBoys
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public class JsonClass
-    {
+       public class TestDataGrid
+       {
+        public string  CheckName { get; set; }
+        public string  Buyer { get; set; }
 
-        
-        public string user { get; set; }
-        public int totalSum { get; set; }
-        public DateTime localDateTime { get; set; }
-        public JsonItems[] items { get; set; }
-
-        public string nameTable;
-        public string NameTable()
-        {
-            if (this.user.Contains("Агроторг") || this.user.Contains("АГРОТОРГ"))
-            {
-                this.nameTable = "Пятёрочка " + this.localDateTime.ToString("dd.MM-hh:mm");
-                return this.nameTable;
-            }
-            if (this.user.Contains("Прайс") || this.user.Contains("ПРАЙС"))
-            {
-                this.nameTable = "ФиксПрайс " + this.localDateTime.ToString("dd.MM-hh:mm");
-                return this.nameTable;
-            }
-            if (this.user.Contains("Лента") || this.user.Contains("ЛЕНТА"))
-            {
-                this.nameTable = "Лента " + this.localDateTime.ToString("dd.MM-hh:mm");
-                return this.nameTable;
-            }
-            if (this.user.Contains("Перекресток") || this.user.Contains("ПЕРЕКРЕСТОК"))
-            {
-                this.nameTable = "Перекресток " + this.localDateTime.ToString("dd.MM-hh:mm");
-                return this.nameTable;
-            }
-            if (this.user.Contains("Магнит") || this.user.Contains("МАГНИТ"))
-            {
-                this.nameTable = "Магнит " + this.localDateTime.ToString("dd.MM-hh:mm");
-                return this.nameTable;
-            }
-            if (this.user.Contains("Дикси") || this.user.Contains("ДИКСИ"))
-            {
-                this.nameTable = "Дикси " + this.localDateTime.ToString("dd.MM-hh:mm");
-                return this.nameTable;
-            }
-            else
-                return "Error";
-        }
-    }
-
-    public class JsonItems
-    {
-        public string name { get; set; }
-        public int sum { get; set; }
-    }
+       }
 
     public partial class MainWindow : Window
     {
-        static SqliteConnection connectionPBData = new SqliteConnection("Data Source = PBDataTest.db");
+        List<TestDataGrid> test = new List<TestDataGrid>();
+
+        static readonly SqliteConnection connectionPBData = new SqliteConnection("Data Source = PBDataTest.db");
         public MainWindow()
         {
             InitializeComponent();
@@ -92,27 +48,43 @@ namespace PayBoys
             command.ExecuteNonQuery();
 
             //создание таблицы для хранения названий чеков
-            command.CommandText = "CREATE TABLE IF NOT EXISTS Checks (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, ChecksName TEXT NOT NULL)";
+            command.CommandText = "CREATE TABLE IF NOT EXISTS Checks (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, ChecksName TEXT NOT NULL, Buyer TEXT NOT NULL DEFAULT \'Eugene\')";
             command.ExecuteNonQuery();
+
+            command.CommandText = "SELECT * FROM Checks";
+            command.ExecuteNonQuery();
+
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows) // если есть данные
+                {
+                    while (reader.Read())   // построчно считываем данные
+                    {
+                        TestDataGrid t = new TestDataGrid();
+                        t.CheckName = reader.GetString(1);
+                        t.Buyer = reader.GetString(2);
+                        test.Add(t);
+                    }
+                }
+            }
+            dataTable.ItemsSource = test;
         }
 
-        
         private void Button_Click_LoadJson(object sender, RoutedEventArgs e)
         {
            
             var f = new OpenFileDialog();
             if (f.ShowDialog().GetValueOrDefault(true))
-            {
-                var jsonString = File.ReadAllText(f.FileName);
-                JsonClass jSon = JsonSerializer.Deserialize<JsonClass>(jsonString);
+            {                
+                JsonClass check = JsonSerializer.Deserialize<JsonClass>(File.ReadAllText(f.FileName));
                               
                 SqliteCommand command = new SqliteCommand();
                                        
                 command.Connection = connectionPBData;
-                command.CommandText = $"CREATE TABLE IF NOT EXISTS \"{jSon.NameTable()}\" (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, BoB TEXT NOT NULL)";
+                command.CommandText = $"CREATE TABLE IF NOT EXISTS \"{check.NameTable()}\" (_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Product TEXT NOT NULL)";
                 command.ExecuteNonQuery();
 
-                command.CommandText = $"INSERT INTO Checks (ChecksName) VALUES (\"{jSon.nameTable}\")";
+                command.CommandText = $"INSERT INTO Checks (ChecksName, Buyer) VALUES (\"{check.nameTable}\", \"unknown\")";
                 command.ExecuteNonQuery();
             }
         }
